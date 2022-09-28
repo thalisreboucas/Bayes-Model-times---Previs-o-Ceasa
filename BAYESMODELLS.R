e2c4d976741eb5b4b219fb2fc79c671ed3aa3b34
Thalis Rebouças de Oliveira <50429343+thalisreboucas@users.noreply.github.com>
2022-09-28 14:52:24 -0300

    Add files via upload

--- BAYESMODELLS.R
+++ BAYESMODELLS.R
@@ -1,35 +1,40 @@
-library(readxl)
-library(plotly)
-library(tidymodels)
-library(bayesmodels)
-library(modeltime)
-library(tidyverse)
-library(timetk)
-library(lubridate)
-library(datawizard)
+library(pacman)
+
+pacman::p_load(readxl,# Open Data in Xl (Excel)
+               plotly, # Making dynamic graphs
+               tidymodels, # Model statistics
+               bayesmodels, # Bayes times models 
+               modeltime, # Times model classics 
+               tidyverse, # All tidy for manipulation
+               timetk, # Making a tables and graph's of timeseries
+               lubridate, # Working if date
+               datawizard # making a spells on the datas.
+               )
 
 
-Dados_Ceasa_Preco <- read_excel("E:/edime/Thalis/MEU/Ceasa/Dados_Ceasa_Preco.xlsx", 
+# Open dataset 
+Dados_Ceasa_Preco <- readxl::read_excel("E:/edime/Thalis/MEU/Ceasa/Dados_Ceasa_Preco.xlsx", 
                                 col_types = c("text", "text", "text", 
                                               "skip", "skip", "skip", "numeric", 
                                               "date"))
-
+# Separate the datas what i want
 data <-  Dados_Ceasa_Preco %>% dplyr::select(ID_Produto,Data,`Preco Medio (R$)`) %>%
   set_names(c("id", "date", "value"))
 
 
+# Choosing the product what i want
 df <- data %>% group_by(id) %>%
     filter(id == 24)
-  
 
-itens_abc <- c(24,7,8,27,34,44,33,16)
+#Nothing Important
+#itens_abc <- c(24,7,8,27,34,44,33,16)
 
 
-# Separação ----
+# Splits of test data ----
   
 splits <- df %>% initial_time_split(prop = 0.9)
 
-# AED -----
+# AED and Graph function -----
 
 graph <- function(id_i ){
   
@@ -43,7 +48,6 @@ graph <- function(id_i ){
   
 }
 
-graph(24)
 
 table <- function(id_i ){
   
@@ -51,43 +55,45 @@ table <- function(id_i ){
   
 }
 
+graph(24)
 table(24)
 
+# :-)
 
-## Teste
+## Making the models of timeseries 
+# BAYEEESSS !!!!
 
-
+# Sarima: bayesmodels connects to the bayesforecast package .
+# Integrated AutoRegressive Moving Averages with Seasonality.
 model_fit_arima_bayes<- sarima_reg() %>%
   set_engine(engine = "stan",tune()) %>%
   fit(value ~ date + month(date,label = T), data = training(splits))
 
-
-plot(model_fit_arima_bayes$fit$models$model_1)
-
-
+# Garch: bayesmodels connects to the bayesforecast package.
+# volatility modeling
 model_fit_garch <- garch_reg() %>% 
   set_engine("stan") %>% 
   fit(value ~ date + month(date,label = T), data = training(splits))
 
-plot(model_fit_garch$fit$models$model_1)
-
+#Adaptive Splines Surface: bayesmodels connects to the BASS package.
 model_fit_adps <- adaptive_spline() %>% 
   set_engine("stan") %>% 
   fit(value ~ date + month(date,label = T), data = training(splits))
 
 
 
+# Tables of the models
 models_tbl <- modeltime_table(
   model_fit_arima_bayes,
   model_fit_garch,
   model_fit_adps
 )
 
-
+# Calibration table
 calibration_tbl <- models_tbl %>%
   modeltime_calibrate(new_data = testing(splits))
 
-
+# Graph of the forecasting.
 calibration_tbl %>%
   modeltime_forecast(
     new_data    = testing(splits),
@@ -98,20 +104,23 @@ calibration_tbl %>%
     .interactive      = T
   )
 
-
+# Calibration Table do see the R^2 
 calibration_tbl %>%
   modeltime_accuracy() %>%
   table_modeltime_accuracy(
-    .interactive = interactive
+    .interactive = T
   )
 
 
+# Refti to do the Forecasting
 refit_tbl <- calibration_tbl %>%
   modeltime_refit(data = df)
 
+# Forecastings Graph !!!
 refit_tbl %>%
   modeltime_forecast(h = "6 month", actual_data = df) %>%
   plot_modeltime_forecast(
     .legend_max_width = 25, # For mobile screens
     .interactive      = T )
-"# Bayes-Model-times---Previs-o-Ceasa" 
+
+# Done.
